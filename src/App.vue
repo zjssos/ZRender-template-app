801e24df003fa07a639a89db75b200e577449d2a
zhaojiasong <zhaojiasong@yowshu.com>
2022-04-19 18:41:13 +0800

    feat: 初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="container"></div>
+</template>
+<script setup>
+import * as zrender from "zrender";
+import {nextTick, onMounted} from "vue";
+
+onMounted(()=>{
+    nextTick(()=>{
+        const dom = document.getElementById('container')
+        const zr = zrender.init(dom);
+        const circle = new zrender.Circle({
+            shape: {
+                cx: 150,
+                cy: 50,
+                r: 40
+            },
+            style: {
+                fill: 'none',
+                stroke: '#F00'
+            }
+        });
+        zr.add(circle);
+    })
+
+})
+</script>
+
+
+
+<style>
+#container{
+    width: 1000px;
+    height: 1000px;
+}
+</style>
